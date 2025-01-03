111290baf025f004295cab94d4bb5640957410c3
Ton van de Ven <threehook@users.noreply.github.com>
2025-01-03 15:08:17 +0100

    Tf api token (#52)
    * feat: terraform after new tf_api_token
    
    * feat: tf_api_token in destroy
    
    * feat: tf_api_token in destroy
    
    * feat: tf_api_token in destroy
    
    * feat: tf_api_token in destroy
    
    * feat: tf_api_token in destroy
    
    * feat: ENVIRONMENT without env.
    
    * feat: ENVIRONMENT with vars prefix.
    
    * feat: remove env vars ENVIRONMENTS
    
    * feat: remove env vars ENVIRONMENTS
    
    * feat: remove env vars ENVIRONMENTS
    
    * feat: remove env vars ENVIRONMENTS

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,3 @@
-variable "environment" {
-  description = "Name of the environment"
-}
 
 terraform {
   required_providers {
@@ -24,24 +21,19 @@ resource "github_membership" "membership_threehook" {
   username = "threehook"
 }
 
-variable "kube_config" {
-  type        = string
-  description = "The kubernetes config"
-}
-
-provider "kubernetes" {
-  config_path = var.kube_config
-
-  experiments {
-    manifest_resource = true
-  }
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = var.kube_config
-  }
-}
+# provider "kubernetes" {
+#   config_path = var.kube_config
+#
+#   experiments {
+#     manifest_resource = true
+#   }
+# }
+#
+# provider "helm" {
+#   kubernetes {
+#     config_path = var.kube_config
+#   }
+# }
 
 provider "azurerm" {
   features {}
---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "environment" {
+  description = "Name of the environment"
+  type    = string
+  default = "test"
+}
+
+# variable "kube_config" {
+#   type        = string
+#   description = "The kubernetes config"
+# }
